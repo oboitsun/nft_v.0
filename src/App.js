9cb2467789379dfe465b899f3b96bbacfcdfb28b
Oleksandr Boitsun <thewip3out@gmail.com>
2021-04-15 00:02:19 +0300

    added some animations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
 import Main from './Components/Main'
-import { AnimatePresence } from 'framer-motion'
+
 function App() {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <div className='App'>
-        <Main />
-      </div>
-    </AnimatePresence>
+    <div className='App'>
+      <Main />
+    </div>
   )
 }
 
--- src/Components/WhatIsOrigins.js
+++ src/Components/WhatIsOrigins.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { motion, useAnimation } from 'framer-motion'
