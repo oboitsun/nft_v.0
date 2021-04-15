60c2c0a8712e6c7189094d1747e5116874dcd1f3
Oleksandr Boitsun <thewip3out@gmail.com>
2021-04-15 18:54:00 +0300

    final fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css'
 import Main from './Components/Main'
-import { useEffect, useState } from 'react'
+
 function App() {
-  const [width, changeWidth] = useState(0)
-  useEffect(() => {
-    changeWidth(window.innerWidth)
-  }, [window.innerWidth])
   return (
     <div className='App'>
-      <div className='absolute top-0 left-0'>{width}</div>
       <Main />
     </div>
   )
