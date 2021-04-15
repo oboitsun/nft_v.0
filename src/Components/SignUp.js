3f57fbfce662a322f9b5a6cae2c54663044e39a1
Oleksandr Boitsun <thewip3out@gmail.com>
2021-04-15 18:57:41 +0300

    final fixes

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class SignUp extends Component {
   render() {
     return (
-      <button className='w-full flex justify-center font-mont text-gold font-extrabold  py-4 lg:py-6 lg:px-16 2xl:px-16 bg-btnred rounded-full  sm:text-sm md:text-sm lg:text-xl 2xl:text-4xl leading-none uppercase'>
+      <button className='w-full flex justify-center font-mont text-gold font-extrabold  py-4 px-8 lg:py-6 lg:px-16 2xl:px-16 bg-btnred rounded-full  sm:text-sm md:text-sm lg:text-xl 2xl:text-4xl leading-none uppercase'>
         sign up
       </button>
     )
